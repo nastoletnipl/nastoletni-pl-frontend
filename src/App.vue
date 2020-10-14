dac84d48c241973f75b757682cef671a7c8c62f8
Piotrek <peter@LAPTOP-OACQAF8V>
2020-10-14 18:33:07 +0200

    Router template

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,8 @@
 <template>
-  <Login />
-  <Register />
+
+  <router-view/>
 </template>
 
-<script>
-import Login from './components/Login.vue'
-import Register from "@/components/Register";
-
-export default {
-  name: 'App',
-  components: {
-    Login,
-    Register
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import firebase from 'firebase';
 import firebaseConfig from "../firebaseConfig";
+import router from './router'
 
 firebase.initializeApp(firebaseConfig.data);
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
