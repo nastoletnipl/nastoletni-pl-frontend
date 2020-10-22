0bcb786e6ade7fe4e95c9ff99be1fa78bf6edf4f
Michał Gołda <michalgolda@vp.pl>
2020-10-22 18:38:50 +0200

    Add router and font-awesome icons

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import router from './router'
+
+
+library.add(faUser)
+
+const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+app.use(router)
+app.mount('#app')
